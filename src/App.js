65e4e8fcf5a9953fc810c8d12c85174af5b918ee
Andreis <Derek.Andreis@us.sogeti.com>
2020-01-29 11:20:22 -0500

    uh

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    //State
+    this.state = {
+      employees: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/getSogetiEmployees')
+    .then(employees => employees.json())
+    .then(employees => {
+      console.log(employees)
+    })
+    .catch(err => console.log(err))
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1> Sogeti Employee Directory </h1>
+        <ol>
+        { this.state.employees.map((employee, index) => {
+          return (
+              <li key={index}>{employee.name}</li>
+            )
+          })
+        }
+        </ol>
+      </div>
+    );
+  }
 }
 
 export default App;
